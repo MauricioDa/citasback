8bd375e2e7b165193f9061336e4393ee549653d1
Mauricio.Utrilla <mauriciodanuelud@gmail.com>
2021-05-09 21:28:49 -0500

    se agrega metodo onStartup a la clase principal

--- src/main/java/org/conocer/citas/CitasApplication.java
+++ src/main/java/org/conocer/citas/CitasApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 @SpringBootApplication
 public class CitasApplication  extends SpringBootServletInitializer{
@@ -17,6 +24,22 @@ public class CitasApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CitasApplication.class, args);
+	}
+
+
+	@Override
+	public void onStartup(ServletContext container) throws ServletException {
+		WebApplicationContext context = getContext();
+		ServletRegistration.Dynamic registration = container.addServlet("dispatcher", new DispatcherServlet(context));
+		registration.setLoadOnStartup(1);
+		registration.addMapping("/");
+		super.onStartup(container);
+	}
+
+	private WebApplicationContext getContext() {
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+		context.setConfigLocation(CitasApplication.class.getName());
+		return context;
 	}
 
 }
